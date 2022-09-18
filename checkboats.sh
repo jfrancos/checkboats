94d5d847a505af7beff0f38deed4ff3c0573b226
EC2 Default User <ec2-user@ip-172-31-35-6.us-east-2.compute.internal>
2022-09-18 22:50:21 +0000

    Simplify some stuff

--- checkboats.sh
+++ checkboats.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-LINKWITHBIKE='https://secure.baystatecruisecompany.com/reserve?trip_info=ferry%7Cow%7CProvincetown&departure_date=2022-09-18&adults=1&seniors=0&children=0&babies=0&bikes=1&submit=See+available+trips'
-LINK='https://secure.baystatecruisecompany.com/reserve?trip_info=ferry%7Cow%7CProvincetown&departure_date=2022-09-18&adults=1&seniors=0&children=0&babies=0&bikes=0&submit=See+available+trips'
-INDEX=$(curl -Lsc cookie "$LINK")
-MESSAGE="$(echo "$INDEX" | grep "Fast Ferry, Sun Sep 18, 2022" | sed 's/<[^>]*>//g' | awk 1 ORS='\n\n' )"$'\n'$'\n'"$LINKWITHBIKE"
-KEY=[PRIVATE KEY FROM textbelt.com]
+DATE=2022-09-20
+ADULTS=1
+SENIORS=0
+CHILDREN=0
+BABIES=0
+BIKES=0
+LINK="https://secure.baystatecruisecompany.com/reserve?trip_info=ferry%7Cow%7CProvincetown&departure_date=$DATE&adults=$ADULTS&seniors=$SENIORS&children=$CHILDREN&babies=$BABIES&bikes=$BIKES&submit=See+available+trips"
+INDEX=$(curl -Lsc- "$LINK")
+MESSAGE="$(echo "$INDEX" | grep "Fast Ferry," | sed 's/<[^>]*>//g' | awk 1 ORS='\n\n' )"$'\n'$'\n'"$LINK"
+KEY="$1" # You can put your key here but don't save it to a git repo
 
-#echo "$MESSAGE"
-
-echo -e "$MESSAGE" | md5sum -c boats.md5 || curl -X POST https://textbelt.com/text --data-urlencode phone='6178385268' --data-urlencode message="$MESSAGE" -d key="$KEY" && echo -e "$MESSAGE" | md5sum > boats.md5
+echo "$MESSAGE"
+#echo "$INDEX"
+#echo "$LINK"
+#echo -e "$MESSAGE" | md5sum -c boats.md5 || curl -X POST https://textbelt.com/text --data-urlencode phone='6178385268' --data-urlencode message="$MESSAGE" -d key="$KEY" && echo -e "$MESSAGE" | md5sum > boats.md5
 
