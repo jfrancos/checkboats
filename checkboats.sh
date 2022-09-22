f838439dbfe19d40fe2b4252015652e29581647b
Ubuntu <ubuntu@ip-172-31-11-33.us-east-2.compute.internal>
2022-09-22 03:54:47 +0000

    Use cmp not md5sum

--- checkboats.sh
+++ checkboats.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 DEPARTING=Boston # 'Boston' or 'Provincetown'
 DATE=2022-09-24
 ADULTS=1
@@ -9,14 +10,14 @@ BIKES=0
 LINK="https://secure.baystatecruisecompany.com/reserve"
 
 WEBPAGE="$( curl -GLsc- -w"%{url}\n" "$LINK" \
---data-urlencode trip_info="ferry|ow|$DEPARTING" \
---data-urlencode departure_date="$DATE" \
---data-urlencode adults="$ADULTS" \
---data-urlencode seniors="$SENIORS" \
---data-urlencode children="$CHILDREN" \
---data-urlencode babies="$BABIES" \
---data-urlencode bikes="$BIKES" \
---data-urlencode submit="See+available+trips" \
+  --data-urlencode trip_info="ferry|ow|$DEPARTING" \
+  --data-urlencode departure_date="$DATE" \
+  --data-urlencode adults="$ADULTS" \
+  --data-urlencode seniors="$SENIORS" \
+  --data-urlencode children="$CHILDREN" \
+  --data-urlencode babies="$BABIES" \
+  --data-urlencode bikes="$BIKES" \
+  --data-urlencode submit="See+available+trips" \
 )"
 
 MESSAGE="$( echo "$WEBPAGE" | grep --group-separator="<br><br>" -C0 "Fast Ferry,\|/reserve?" | html2text -width 999 )"
@@ -24,6 +25,10 @@ KEY="$2" # You can put your key here but don't save it to a git repo
 PHONE="$1"
 
 echo "$MESSAGE"
-# can we use diff instead of md5sum
-#echo -e "$MESSAGE" | md5sum -c boats.md5 || curl -X POST https://textbelt.com/text --data-urlencode phone="$PHONE" --data-urlencode message="$MESSAGE" -d key="$KEY" && echo -e "$MESSAGE" | md5sum > boats.md5
+cmp -s <( echo "$MESSAGE" ) message || ( curl -X POST https://textbelt.com/text \
+  --data-urlencode phone="$PHONE" \
+  --data-urlencode message="$MESSAGE" \
+  -d key="$KEY" \
+  && echo "$MESSAGE" > message
+)
 
